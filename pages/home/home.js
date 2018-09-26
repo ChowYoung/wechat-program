7a0174c0ad2a30d9e348cca1337413eda0611c88
Young.周杨 <a6727467@163.com>
2018-09-26 16:40:34 +0800

    首页头部横向标签滚动
    首页头部横向标签滚动

---
+++ pages/home/home.js
@@ -0,0 +1,89 @@
+// pages/home/home.js
+
+import getHomeTagData from '../../data/GetHomeTag.js'
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    goodsOptList: [],
+    activeIndex: 0
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+    this.getGoodsOptList();
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+
+  getGoodsOptList() {
+    const {
+      data
+    } = getHomeTagData;
+    this.setData({
+      goodsOptList: data.goods_opt_list
+    })
+  },
+
+  getGoodsOptDetail(el) {
+    const {
+      oid,
+      index
+    } = el.currentTarget.dataset
+    this.setData({
+      activeIndex: index
+    })
+  }
+})
